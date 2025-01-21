34ad6caf22a0262be06e4b916b14b3bb5407adbc
HumanM00n <mrouy92@gmail.com>
2025-01-21 20:43:39 +0100

    Ajout des différentes fonctionalités piur les utilisateurs : - UPDATE, SELECT, INSERT, DELETE

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import pool from "@/app/_lib/db";
 import bcrypt from 'bcryptjs'; 
+import { ResultSetHeader } from "mysql2";
 
 //Création d'un utilisateur
 export async function POST(req: Request) {
@@ -28,3 +29,92 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'Problème lors de la création de l\'utilisateur' }, { status: 500 });
     }
 }
+
+// Récupération des utilisateurs 
+export async function GET(req: Request) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const userId = searchParams.get('id');
+
+        let query;
+        let values: string[]; 
+
+        if (userId) {
+            query = 'SELECT * tns_users WHERE id_users = ?';
+            values = [userId];
+        } else {
+            query = 'SELECT * from tns_users';
+            values = [];
+        }
+
+        const [rows] = await pool.query(query, values);
+        return NextResponse.json(rows);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Problème lors de la récupération des utilisateurs" })
+    } 
+}
+
+
+    // Mise à jour de l'uilisateur 
+    export async function PUT(req: Request) {
+        const { id, lastName, firstName, email, password, phone_number, city } = await req.json();
+
+    try {
+      let hashedPassword;
+      if (password) {
+        hashedPassword = bcrypt.hash(password, 10);
+      }  
+    
+
+    const query = `UPDATE tns_users
+                   SET
+                     user_lastname = COALESCE(?, user_lastname),
+                     user_firstname = COALESCE(?, user_firstname),
+                     user_email = COALESCE(?, user_email),
+                     user_password = COALESCE(?, user_password),
+                     user_phone_number = COALESCE(?, user_phone_number),
+                     user_city = COALESCE(?, user_city)
+                   WHERE id_users = ?,`;
+
+    const values = [id, lastName, firstName, email, hashedPassword, phone_number, city];
+    const [result] = await pool.execute<ResultSetHeader>(query, values);
+    
+
+    if (result.affectedRows === 0 ) {
+        return NextResponse.json({ message: 'Utilisateur non trouvé' }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: 'Utilisateur mis à jour avec succès!' })
+} catch (error) {
+
+    console.error(error);
+    return NextResponse.json({ message: 'Problème lors de la mise à jour de l\'utilisateur' }, { status: 500 });
+}
+
+}
+
+// Suppression d'un utilisateur 
+export async function DELETE(req: Request) {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('id');
+
+   if (!userId) {
+    return NextResponse.json({ message: 'Id de l\'utilisateur introuvable' }, { status: 400 });
+   } 
+
+   try {
+    const query = 'DELETE FROM tns_users WHERE id_users = ?';
+    const [result] = await pool.execute<ResultSetHeader>(query, [userId]);
+
+    if (result.affectedRows === 0 ) {
+        return NextResponse.json({ message: 'Utilisateur non trouvé' }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: 'Utilisateur supprimé avec succès!' })
+   } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Problème lors de la suppression de l\'utilisateur' });   
+   }
+
+} 
