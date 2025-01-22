6c582e67ff0b4bbf362862ef068844f422d46ba2
Mathis Rouy <c1120@cnetp.fr>
2025-01-22 18:57:14 +0100

    Développement du header

---
+++ app/_components/header.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+// Import du logo 
+// Import du titre de la navbar
+import Link from "next/link";
+
+
+export default function Header() {
+    return (
+        <header>
+            
+        </header>
+    );
+}
