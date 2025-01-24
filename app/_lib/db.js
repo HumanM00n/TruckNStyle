82a978beeef58139f6b237359e696a67b1637487
Mathis Rouy <c1120@cnetp.fr>
2025-01-24 16:58:39 +0100

    Retirage des valeur pour la connexion par des variables d'environnement

--- app/_lib/db.js
+++ app/_lib/db.js
@@ -1,26 +1,14 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'XVsikn92',
-    database: 'truckn_style_bdd',
-    port: 3308,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_HOST,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
-
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_HOST,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
 
 export default pool;
