a5b5f8782feeb9f9c59ab967915f9661f133b962
Mathis Rouy <c1120@cnetp.fr>
2025-01-24 16:58:10 +0100

    Ajout de ClassName dans la balise <header>

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className=" bg">
+        <header className=" ">
             <div className="containerNav ">
                 <div className="containerLogo"></div>
                 <div className="containerNavLinks"></div>
