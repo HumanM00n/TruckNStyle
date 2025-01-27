86f426ea1fb3c9cae72681f468bb41ca786ba38d
Mathis Rouy <c1120@cnetp.fr>
2025-01-27 16:36:01 +0100

    Ajout de : - 'use client'
    Fin de développement visuel de la barre de navigation
    
    Reste à faire :
    - Responsiv design (menu burger,etc..)
    -Liens vers les différentes pages

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,36 @@
+'use client';
+
 import React, { Component } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Header() {
     return (
-        <header className=" bg">
-            <div className="containerNav ">
-                <div className="containerLogo"></div>
-                <div className="containerNavLinks"></div>
-                <div className="containerIcon"></div>
+        <header className=" w-100 h-36 bg-noir flex justify-center font-montserrat text-base">
+            <div className="containerNav w-10/12 h-32 flex flex-row justify-around">
+
+                <div className="containerLogo flex">
+                    <Image
+                        src="/logo/logoTNS-brown.png"     
+                        width={200}
+                        height={150}
+                        quality={90}
+                        alt="Logo du site"
+                    />
+                </div>
+
+                <div className="containerNavLinks h-20 border border-light rounded-sm flex align-center py-6 mt-5 pr-6 pl-6 gap-16">
+                    <Link href="/">Réservations</Link>
+                    <Link href="/">À propos</Link>
+                    <Link href="/">Contactez-nous</Link>
+
+                </div>
+
+                <div className="containerIcon h-20 w-60 flex justify-center gap-8 mt-5 py-6">
+                    <Link href={"/"}>Connexion</Link>
+                    <Link href={"/"}>Inscription</Link>
+                </div>
             </div>
         </header>
     );
