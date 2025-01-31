f9f03cff35be567b557a53134fad8f9e30e7697b
Mathis Rouy <c1120@cnetp.fr>
2025-01-31 11:25:01 +0100

    Mis en commentaire l'omport de bcryptjs

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import pool from "@/app/_lib/db";
 
+export async function POST(req: Request) {
+
+}
