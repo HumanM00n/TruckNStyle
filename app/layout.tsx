47d7215b461afdb95586374a01caf471264b65d4
Mathis Rouy <c1120@cnetp.fr>
2025-02-03 18:34:37 +0100

    Ajout de l'import de bootstrap + Restructuration du code pour le RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,30 @@
-// -------------My CSS-------------------
-import '@/app/styles/globals.css' 
+import '@/app/styles/globals.css';
 
-// ------------------------------FONTAWESOME----------------------------------
+// FontAwesome
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'; // Import du CSS       
-config.autoAddCss = false; // Désactive l'ajout automatique des styles //    
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
-// -----------------HEADER---------------------
-import Header from './_components/header'; 
-
+import Header from './_components/header';
 import type { Metadata } from "next";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// ✅ Metadata fonctionne ici
 export const metadata: Metadata = {
   title: "A définir",
   description: "A définir"
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
       <body>
         <Header />
-
         {children}
       </body>
     </html>
