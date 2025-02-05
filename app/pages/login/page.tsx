44c6d23c905d2bb1f979ff5d74226d4b27b9519a
Mathis Rouy <c1120@cnetp.fr>
2025-02-05 15:34:06 +0100

    UPDATE de la mise en page du formulaire

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -1,47 +1,50 @@
 'use client';
 
-// import { useState } from "react";
-// import loginForm from "@/app/ui/login";
+import { useState } from "react";
 import Link from "next/link";
 
 export default function loginPage() {
     return (
-        <div className="border-2 border-red-500 w-full h-96">
-            <div className="border-1 border-blue-500 flex w-full h-auto">
+        <div className="border-2 border-red-500 w-full max-h-full h-[715px]">
+            <div className="border-1 border-blue-500 flex w-full">
 
-                <form action="" className="border-1 border-green-600 rounded-md bg-[#733E34] w-3/6 flex flex-col justify-center px-32 py-12">
-                    <h1 className="text-light">Connexion</h1>
-                    <div className="grid grid-cols-1 gap-y-6">
-                        <input type="email" className="form-control w-72 py-2 px-3" id="inputForEmail" />
-                        <input type="password" className="form-control w-72" id="" />
-                    </div>
+                <div className="border-1 border-green-600  w-3/6 flex flex-col justify-center ">
+                    <form action="" className="border-1 border-pink-200 bg-[#733E34] py-12 rounded-md px-8">
+                        <h1 className="text-light">Connexion</h1>
+                        <div className="grid grid-cols-1 gap-y-6">
 
-
-                    <div className="">
-                        <Link href={""}>J&apos;ai oublié mon mot de passe </Link>
-                    </div>
-
-                    <div className="btnSubmit">
-                        <button type="button" className="btn btn-outline-light">Se connecter</button>
-                    </div>
+                            <input type="email" className="form-control w-72 
+                            placeholder-[#8C5744] placeholder-opacity-70 focus:outline-none focus:ring-4 focus:ring-[#FBFBFB] 
+                            py-2.5 px-4" id="inputForEmail" placeholder="Email" />
+                            <input type="password" className="form-control w-72 py-2.5 px-4 placeholder-[#8C5744] placeholder-opacity-70" id="inputForPaswword" placeholder="Mot de passe" />
+                        </div>
 
 
-                    <div className="hr">
-                        <hr />
-                    </div>
-                    
-                    <div className="">
-                        <Link href={"#"}>Vous n&apos;avez pas de compte?! Créer en un ! </Link>
-                    </div>
+                        <div className="">
+                            <Link href={""}>J&apos;ai oublié mon mot de passe </Link>
+                        </div>
 
-                    <div className="redirectRegister">
-
-                    </div>
-                </form>
+                        <div className="btnSubmit">
+                            <button type="button" className="btn btn-outline-light">Se connecter</button>
+                        </div>
 
 
+                        <div className="hr">
+                            <hr />
+                        </div>
 
-                
+                        <div className="">
+                            <Link href={"#"}>Vous n&apos;avez pas de compte ? Créer en un ! </Link>
+                        </div>
+
+                        <div className="redirectRegister">
+
+                        </div>
+                    </form>
+                </div>
+
+
+
                 <div className=""></div>
             </div>
         </div>
