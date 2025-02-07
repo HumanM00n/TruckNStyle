abc44da6b4a792bc105599c92785a007024f2c69
Mathis Rouy <c1120@cnetp.fr>
2025-02-07 16:02:16 +0100

    Développement en cours

---
+++ app/_components/footer.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+export default function Footer() {
+
+    return (
+        <footer className="border-2 border-red-600">
+            <div className=""></div>
+
+            <div className=""></div>
+
+            <div className="">
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+
+        </footer>
+    );
+}
