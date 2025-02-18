4956f0dc276842e476504373127aea1b44a2ee21
HumanM00n <mrouy92@gmail.com>
2025-02-18 23:16:59 +0100

    Dev de la page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,21 @@
-import { notFound } from "next/navigation";
-import { headers } from 'next/headers';
+import Link from 'next/link'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTruck } from '@fortawesome/free-solid-svg-icons'
 
 export default function NotFound() {
-    return (
-        <div className="not-found">
-            <h2>404 erreur || Test</h2>
-            <p>Cette page est introuvable</p>
-        </div>
-    )
+  return (
+    <div className='flex flex-col justify-center items-center text-white gap-3'>
+      <Image 
+      src={"/logo/logoTNS-white.png"}
+      width={250}
+      height={100}
+      alt="Logo blanc de l'application"
+      className=''
+      />
+      <h2>Erreur 404: Cette page est sorti du parking </h2>
+      <p>Nos développeurs sont sur le coup... ou peut-être pas. <FontAwesomeIcon icon={faTruck} /></p>
+      <Link href="/">Page d&apos;accueil</Link>
+    </div>
+  )
 }
