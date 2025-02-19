7d79737be0e638819d7edf8c7aa7b802ada5a0fb
Mathis Rouy <c1120@cnetp.fr>
2025-02-19 18:56:42 +0100

    Ajout de la Metadata

--- app/(error_layout)/layout.tsx
+++ app/(error_layout)/layout.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "404 | Not found",
+  description: "A définir"
+};
+
 export default function ErrorLayout({ children }: { children: React.ReactNode }) {
     return <main className="flex items-center justify-center min-h-screen bg-gray-100 text-black">{children}</main>;
   }
