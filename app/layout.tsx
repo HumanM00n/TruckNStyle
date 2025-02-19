ade59649c0c2af3a7979da71c042bdd3f78e7074
Mathis Rouy <c1120@cnetp.fr>
2025-02-19 18:56:54 +0100

    Nothing

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,21 +8,21 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
-    title: "A définir",
-    description: "A définir"
+  title: "A définir",
+  description: "A définir"
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <head>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
-            </head>
-            <body>
-                <main>{children}</main>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 }
