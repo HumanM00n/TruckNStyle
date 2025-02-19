366fcd8f81f2a2a122536e7dcb9d7ce85ce30f22
Mathis Rouy <c1120@cnetp.fr>
2025-02-19 17:02:38 +0100

    Ajout du dossier avec un layout imbriqué et stylisé pour ne afficher ni le Header ni le Footer

---
+++ app/(error_layout)/layout.tsx
@@ -0,0 +1,4 @@
+export default function ErrorLayout({ children }: { children: React.ReactNode }) {
+    return <main className="flex items-center justify-center min-h-screen bg-gray-100 text-black">{children}</main>;
+  }
+  
