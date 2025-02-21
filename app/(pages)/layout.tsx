ec9dff840f7f53bef81fb657f1fc69808948cb2a
Mathis Rouy <c1120@cnetp.fr>
2025-02-21 16:00:22 +0100

    Ajout du code pour récupérer les paramètre dans l'URL et de le mettre dans le titre pour les pages qui sont couverts par ce layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,12 +1,36 @@
+"use client";
+
 import Header from "../_components/header";
 import Footer from "../_components/footer";
+import { usePathname } from "next/navigation";
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+
+  const router = usePathname();
+
+  // Récupération de l'URL && On split l'url en 2 && On récupère le deuxième élément de la chaine de caractère 
+  const myUrl = router;
+  const pathnameUrl = myUrl.split("/");
+  const titlePage = pathnameUrl[1] || "Accueil";
+
+  console.log(titlePage);
+
   return (
-    <main>
-      <Header />
-      <section>{children}</section>
-      <Footer />
-    </main>
+    <>
+      <head>
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+          <title>{titlePage}</title>
+        </head>
+      </head>
+      <main>
+        <Header />
+        <section>{children}</section>
+        <Footer />
+      </main>
+    </>
   );
 }
+
