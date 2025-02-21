81afa359e5863cf25c1c29af7845d2b3d71e67eb
Mathis Rouy <c1120@cnetp.fr>
2025-02-21 17:21:59 +0100

    UPDATE

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -18,12 +18,10 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
   return (
     <>
       <head>
-        <head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
-          <title>{titlePage}</title>
-        </head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+        <title>{titlePage}</title>
       </head>
       <main>
         <Header />
